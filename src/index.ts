1cd6ba1e251dac748f9bafd364ddad9fc0bc8f74
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-05-28 22:18:48 -0400

    breakpoint chapter 10

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,56 @@
-class Lesson {
-	subject: string;
+// is type guard
+interface Comedian {
+	funny: boolean;
+}
 
-	constructor(subject: string) {
-		this.subject = subject;
+interface StandupComedian extends Comedian {
+	routine: string;
+}
+
+function isStandupComedian(value: Comedian): value is StandupComedian {
+	return "routine" in value;
+}
+
+function workWithComedian(value: Comedian) {
+	if (isStandupComedian(value)) {
+		// Type of value: StandupComedian
+		console.log(value.routine); // Ok
+	}
+	console.log(value.funny); // Ok
+	// console.log(value.routine); // Error
+}
+
+// keyof and typeof
+const colorExample = {
+	red: 255,
+	green: 0,
+	blue: 0,
+	alpha: 1,
+};
+
+function getRgbColorValue(
+	color: typeof colorExample,
+	key: keyof typeof colorExample
+) {
+	return color[key];
+}
+
+const red = getRgbColorValue(colorExample, "alpha"); // Ok
+
+// asserting errors
+try {
+	// do something
+} catch (error) {
+	if (error instanceof Error) {
+		console.log(error.message);
 	}
 }
 
-class OnlineLesson extends Lesson {
-	url: string;
+// type assertion shorthand
+const map = new Map<string, number>();
 
-	constructor(subject: string, url: string) {
-		super(subject);
-		this.url = url;
-	}
-}
+map.set("foo", 1);
 
-let lesson: Lesson;
-lesson = new Lesson("coding"); // Ok
-lesson = new OnlineLesson("coding", "oreilly.com"); // Ok
+const b = map.get("foo")!; // Ok
 
-let online: OnlineLesson;
-online = new OnlineLesson("coding", "oreilly.com"); // Ok
-// online = new Lesson("coding"); // Error
+b.toFixed(2); // Ok
