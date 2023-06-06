9c971ac7a741046ef716af70d93732b2a36b4504
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-06-06 01:06:41 -0400

    fininshed learning ts

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ map.set("foo", 1);
 const b = map.get("foo")!; // Ok
 b.toFixed(2); // Ok
 
-// generics
+// generic functions
 function identity<T>(input: T) {
 	return input;
 }
@@ -61,7 +61,6 @@ const myString = identity("me"); // Type: "me"
 const myNumber = identity(123); // Type: 123
 const myBoolean = identity(true); // Type: true
 
-//
 function logWrapper<Input>(callback: (input: Input) => void) {
 	return (input: Input) => {
 		console.log("Input:", input);
@@ -79,7 +78,7 @@ logWrapper<string>((input) => {
 });
 
 // Type: (input: unknown) => void
-logWrapper((input) => {
+logWrapper((_input) => {
 	// console.log(input.length);
 	// Error: Property 'length' does not exist on type 'unknown'.
 });
@@ -89,3 +88,138 @@ function makeTuple<First, Second>(first: First, second: Second) {
 }
 
 let tuple = makeTuple(true, "abc"); // Type of value: readonly [boolean, string]
+
+// basic interface example generic
+interface Box<T> {
+	inside: T;
+}
+
+const stringBox: Box<string> = {
+	inside: "hello",
+};
+
+// generic interface
+interface BasicNode<T> {
+	next?: BasicNode<T>;
+	value: T;
+}
+
+function getLast<T>(node: BasicNode<T>): T {
+	if (node.next === undefined) {
+		return node.value;
+	}
+	return getLast(node.next);
+}
+
+const node: BasicNode<string> = {
+	value: "foo",
+	next: {
+		value: "bar",
+	},
+};
+
+const value = getLast(node);
+
+// generic default
+class BasicNodeClass<T = number> {
+	next?: BasicNodeClass<T>;
+	value: T;
+
+	constructor(value: T) {
+		this.value = value;
+	}
+
+	getLast(): T {
+		if (this.next === undefined) {
+			return this.value;
+		}
+		return this.next.getLast();
+	}
+}
+
+const nodeClass = new BasicNodeClass("foo");
+nodeClass.next = new BasicNodeClass("bar");
+const valueClass = nodeClass.getLast();
+
+const nn = new BasicNodeClass(0);
+
+// generics types defaults only can go at the end
+function correctExample<T = number, U = string>(input: T, another: U) {}
+
+// incorrect
+// function incorrectExample<T = number, U>(input: T, another: U = "foo") {}
+
+interface WithLength {
+	length: number;
+}
+
+function somethingWithLength<T extends WithLength>(arg: T) {
+	console.log(arg.length);
+}
+
+somethingWithLength("foo"); // Ok
+somethingWithLength([1, 2, 3]); // Ok
+somethingWithLength({ length: 10 }); // Ok
+//somethingWithLength(10); // Error
+
+/* Funtime */
+
+/*
+The first function constrains the key parameter to be a valid key of the object type, 
+allowing the return type to be inferred based on the specific key provided. 
+
+The second function allows any key from the object type to be passed but infers 
+the return type as a union of all possible property types.
+*/
+function get<T, Key extends keyof T>(obj: T, key: Key) {
+	return obj[key];
+}
+
+function get2<T>(obj: T, key: keyof T) {
+	return obj[key];
+}
+
+const obj = {
+	foo: "hello",
+	bar: [1, 2, 3],
+};
+
+const favorite = get(obj, "foo"); // Type: string
+const vvv = get2(obj, "foo"); // Type: string | number[]
+
+const numbers = get(obj, "bar");
+
+// const avoid = get(obj, "baz"); // Error
+
+// mapped types
+
+// using type
+type Animals = "alligator" | "baboon" | "cat";
+
+type AnimalCounts = {
+	[K in Animals]: number;
+};
+
+const animalCounts: AnimalCounts = {
+	alligator: 5,
+	baboon: 12,
+	cat: 3,
+};
+
+// using Record
+const bbb: Record<Animals, number> = {
+	alligator: 5,
+	baboon: 12,
+	cat: 3,
+};
+
+// using interface
+interface Animals2 {
+	alligator: number;
+	baboon: number;
+	cat: number;
+}
+
+type AnimalCounts2 = {
+	[K in keyof Animals2]: number;
+};
